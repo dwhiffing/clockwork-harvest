bd4332688c89d88c18be3607254fcb5c97edd0a5
Daniel Whiffing <danielwhiffing@gmail.com>
2023-01-09 11:15:06 -0500

    Fixes baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import replace from '@rollup/plugin-replace';
+import { defineConfig } from 'vite'
+import replace from '@rollup/plugin-replace'
 
 export default defineConfig({
+  base: '/clockwork-harvest',
   build: {
     rollupOptions: {
       plugins: [
@@ -12,9 +13,9 @@ export default defineConfig({
           'typeof EXPERIMENTAL': "'true'",
           'typeof PLUGIN_CAMERA3D': "'false'",
           'typeof PLUGIN_FBINSTANT': "'false'",
-          'typeof FEATURE_SOUND': "'true'"
-        })
-      ]
-    }
-  }
-});
+          'typeof FEATURE_SOUND': "'true'",
+        }),
+      ],
+    },
+  },
+})
